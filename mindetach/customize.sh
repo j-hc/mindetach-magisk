6069d6a5ea60d71ed1d278943c3f423ed0dd3d7e
j-hc <25510067+j-hc@users.noreply.github.com>
2022-08-12 10:53:41 +0300

    hopefully fix issues

--- mindetach/customize.sh
+++ mindetach/customize.sh
@@ -1,14 +1,31 @@
-# shellcheck disable=SC2148,SC2086,SC2115
+# shellcheck disable=SC2148,SC2086
 if [ "$ARCH" = "arm" ]; then
-  mv -f $MODPATH/sqlite3-arm $MODPATH/sqlite3
+	mv -f $MODPATH/sqlite3-arm $MODPATH/sqlite3
 elif [ "$ARCH" = "arm64" ]; then
-  mv -f $MODPATH/sqlite3-arm64 $MODPATH/sqlite3
+	mv -f $MODPATH/sqlite3-arm64 $MODPATH/sqlite3
 else
-  abort "ERROR: unsupported arch: ${ARCH}"
+	abort "ERROR: unsupported arch: ${ARCH}"
 fi
 
 rm $MODPATH/sqlite3-*
 chmod +x $MODPATH/sqlite3
 
-ui_print "- Detaching..."
-. $MODPATH/service.sh
+MODDIR=$MODPATH
+. $MODPATH/utils.sh
+
+APPS=$(get_apps) && {
+	ui_print "- Disabling Auto-Update.."
+	am force-stop com.android.vending
+	if ! op=$(disable_au "$APPS"); then
+		ui_print "- WARNING: fail"
+		ui_print "    $op"
+	fi
+
+	ui_print "- Detaching.."
+	if ! op=$(detach "$APPS"); then
+		ui_print "- WARNING: fail"
+		ui_print "    $op"
+	fi
+}
+
+ui_print "  by j-hc (github.com/j-hc)"
