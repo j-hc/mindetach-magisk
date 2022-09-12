39b7882964d8deba4e4d0239ec427b2f169b6888
j-hc <25510067+j-hc@users.noreply.github.com>
2022-09-12 23:06:56 +0300

    add explanations

--- mindetach/customize.sh
+++ mindetach/customize.sh
@@ -13,19 +13,36 @@ chmod +x $MODPATH/sqlite3
 MODDIR=$MODPATH
 . $MODPATH/utils.sh
 
+succes=true
 APPS=$(get_apps) && {
 	ui_print "- Disabling Auto-Update.."
 	am force-stop com.android.vending
 	if ! op=$(disable_au "$APPS"); then
-		ui_print "- WARNING: fail"
+		succes=false
+		ui_print "- WARNING:"
 		ui_print "    $op"
+		ui_print ""
 	fi
 
 	ui_print "- Detaching.."
 	if ! op=$(detach "$APPS"); then
-		ui_print "- WARNING: fail"
+		succes=false
+		ui_print "- WARNING:"
 		ui_print "    $op"
+		ui_print ""
 	fi
 }
 
+if [ $succes = false ]; then
+	if [ -d /data/data/com.android.vending ]; then
+		ui_print "- Do not clean the data of Play Store"
+		ui_print "- Open Play Store and reflash mindetach again!"
+	else
+		ui_print "- Immediate detach was not successful"
+		ui_print "- This may be because you are not using Magisk Manager"
+		ui_print "- mindetach will only work after a reboot!"
+	fi
+fi
+
+ui_print ""
 ui_print "  by j-hc (github.com/j-hc)"
